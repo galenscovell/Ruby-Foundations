97a0f296fe1f6cc063e5066671c6e618ba3898a2
Galen Scovell <galen.scovell@gmail.com>
2014-09-28 20:07:26 -0700

    TicTacToe Update
    Still lacks win conditions check (except draw) and AI.

--- Games/TicTacToe.rb
+++ Games/TicTacToe.rb
@@ -3,75 +3,116 @@
 # Without AI, for now
 
 class Player
+  attr_reader :name, :symbol
+  attr_accessor :wins
 
   def initialize(name, symbol)
     @name = name
     @symbol= symbol
     @wins = 0
-    @losses = 0
-    puts "A new challenger has appeared! #{name} batting for #{symbol}!"
   end
-
 end
 
 class Board
 
   def initialize
-    @board_arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    @game_over = false
+    @board_arr = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
   end
 
   def show
-    p @board_arr[0]
-    p @board_arr[1]
-    p @board_arr[2]
+    puts "\n"
+    @board_arr.each do |subarray|
+      puts "     | " + subarray.join(" ") + " |"
+    end
   end
 
   def update(symbol, row, column)
     @board_arr[row - 1][column - 1] = symbol
+    $board.show
   end
 
   def check_win
-
+    return false
   end
 
-  def player_move(symbol)
-    row_choice = 0
-    until row_choice.between?(1, 3)
-      puts "Enter row choice: "
-      row_choice = gets.chomp.to_i
+  def check_move(player, symbol, row, column) 
+    if @board_arr[row - 1][column - 1] == "X" || @board_arr[row - 1][column - 1] == "O"
+      puts "That spot is already taken."
+      player_move(player, symbol)
+    else
+      update(symbol, row, column)
     end
-    column_choice = 0
-    until column_choice.between?(1, 3)
-      puts "Enter column choice: "
-      column_choice = gets.chomp.to_i
-    end
-    board.update(symbol, row_choice, column_choice)
-    board.show
   end
 
+  def player_move(player, symbol)
+    puts "\n#{player}'s turn (#{symbol}'s)."
+    move_choice = [0, 0]
+    until move_choice[0].between?(1, 3) && move_choice[1].between?(1, 3)
+      puts "Place #{symbol} where? (row column) "
+      input = gets.chomp.split(" ")
+      move_choice[0] = input[0].to_i
+      move_choice[1] = input[1].to_i
+    end
+    check_move(player, symbol, move_choice[0], move_choice[1])
+  end
 end
 
-puts "Player X Name?"
+def play_again(player_1, player_2)
+  puts "\nPlayer 1 wins: #{player_1.wins}."
+  puts "Player 2 wins: #{player_2.wins}."
+  puts "\nPlay again?"
+  again = gets.chomp.upcase
+  if again[0] == "Y"
+    game_play(player_1, player_2)
+  else
+    exit
+  end
+end
+
+# Main game flow
+def game_play(player_1, player_2)
+  $board = Board.new
+  $board.show
+  remaining_moves = 9
+
+  until $board.check_win
+    while remaining_moves > 0
+      $board.player_move(player_1.name, player_1.symbol)
+      if $board.check_win
+        puts "Player one wins!"
+        player_1.wins += 1
+        play_again(player_1, player_2)
+      end
+      remaining_moves -= 1
+      if remaining_moves == 0
+        break
+      end
+      $board.player_move(player_2.name, player_2.symbol)
+      $board.check_win
+      if $board.check_win
+        puts "Player two wins!"
+        player_2.wins += 1
+        play_again(player_1, player_2)
+      end
+      remaining_moves -= 1
+    end
+    puts "No moves left. Draw!"
+    break
+  end
+  play_again(player_1, player_2)
+end
+
+# Player setup
+puts "\nThis is Tic-Tac-Toe, the game of pure skill."
+
+puts "\nWho commands the X's?"
 name_1 = gets.chomp
 symbol_1 = "X"
-player_one = Player.new(name_1, symbol_1)
+player_1 = Player.new(name_1, symbol_1)
 
-puts "Player O Name?"
+puts "Who sends forth the O's?"
 name_2 = gets.chomp
 symbol_2 = "O"
-player_two = Player.new(name_2, symbol_2)
+player_2 = Player.new(name_2, symbol_2)
 
-board = Board.new
-board.show
-
-until board.check_win
-  board.player_move(symbol_1)
-  board.check_win
-  board.player_move(symbol_2)
-  board.check_win
-end
-
-
-
-
+game_play(player_1, player_2)
